a77b052bcebf1053932952402a260b349253d70b
SriNavinM <srinavin99@gmail.com>
2025-06-12 14:30:53 +0530

    REST api done

---
+++ task-manager/public/todo.js
@@ -0,0 +1,242 @@
+class Task {
+    constructor(title, description, dueDate, completed = false) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.completed = completed;
+    }
+
+    markCompleted() {
+        this.completed = true;
+    }
+
+    display(index) {
+        return `
+            <tr class="task ${this.completed ? "completed" : ""}">
+                <td>${this.title}</td>
+                <td>${this.description}</td>
+                <td>${this.dueDate}</td>
+                <td id="${this.completed ? "completed" : "pending"}">${this.completed ? "Completed" : "Pending"}</td>
+                <td>
+                    ${!this.completed ? `<button onclick="completeTask(${index})">Done</button>` : ""}
+                    ${!this.completed ? `<button onclick="editTask(${index})">Update</button>` : ""}
+                    <button onclick="deleteTask(${index})">Delete</button>
+                </td>
+            </tr>
+        `;
+    }
+}
+
+class TaskManager {
+    constructor() {
+        fetch('/api/tasks')
+            .then(response => response.json())
+            .then(data => {
+                this.taskList = data.map(x => new Task(x.title, x.description, x.dueDate, x.completed));
+                this.update();
+            })
+            .catch(err => console.error("Failed to load tasks", err));
+    }
+
+    async addTask(title, description, dueDate) {
+        if (this.taskList.some(task => task.title.toLowerCase() === title.toLowerCase())) {
+            const error = document.getElementById("err-msg");
+            error.textContent = "Task with this title already exists!";
+            error.style.display = "block";
+            return false;
+        }
+        const newTask = { title, description, dueDate, completed: false };
+
+        try {
+            const response = await fetch("/api/tasks", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newTask)
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to save task");
+            }
+
+            this.taskList.push(new Task(title, description, dueDate, false));
+            return true;
+        } catch (err) {
+            console.error(err);
+            const error = document.getElementById("err-msg");
+            error.textContent = "Error saving task!";
+            error.style.display = "block";
+            return false;
+        }
+        return true;
+    }
+
+    async completeTask(index) {
+        const task = this.taskList[index];
+        try {
+            const response = await fetch(`/api/tasks/${index}`, {
+                method: 'PATCH',
+                headers: { "Content-Type": 'application/json' },
+                body: JSON.stringify({ completed: true })
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to update task");
+            }
+
+            task.markCompleted();
+            this.update();
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    async deleteTask(index) {
+        try {
+            const response = await fetch(`/api/tasks/${index}`, {
+                method: 'DELETE'
+            });
+            if (!response.ok) {
+                throw new Error("Failed to delete task");
+            }
+
+            this.taskList.splice(index, 1);
+            this.update();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    update() {
+        const filter = document.getElementById("statusFilter")?.value || "all";
+        const search = document.getElementById("searchInput")?.value.toLowerCase() || "";
+        const filteredTasks = this.taskList
+            .map((task, originalIndex) => ({ task, originalIndex }))
+            .filter(({ task }) => {
+                const matchStatus = filter === "all" || (filter === "pending" && !task.completed) || (filter === "completed" && task.completed);
+                const matchSearch = task.title.toLowerCase().includes(search);
+
+                return (matchSearch && matchStatus);
+            })
+            .sort((a, b) => {
+                return a.task.completed - b.task.completed;
+            })
+
+        document.getElementById("tableBody").innerHTML = filteredTasks
+            .map(({ task, originalIndex }) => task.display(originalIndex))
+            .join("");
+    }
+
+    async updateTask(index, newTitle, newDesc, newDueDate) {
+        if (this.taskList.some((task, i) => i !== index && task.title.toLowerCase() === newTitle.toLowerCase())) {
+            const error = document.getElementById("err-msg");
+            error.textContent = "Task with this title already exists!";
+            error.style.display = "block";
+            return false;
+        }
+
+        try {
+            const response = await fetch(`/api/tasks/${index}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title: newTitle, description: newDesc, dueDate: newDueDate })
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to update task");
+            }
+
+            this.taskList[index].title = newTitle;
+            this.taskList[index].description = newDesc;
+            this.taskList[index].dueDate = newDueDate;
+            this.update();
+            return true;
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+}
+
+const handler = new TaskManager();
+
+function completeTask(index) {
+    handler.completeTask(index);
+    handler.update();
+}
+
+function deleteTask(index) {
+    handler.deleteTask(index);
+    handler.update();
+}
+
+let currentUpdateIndex = null;
+
+function openModal(isEdit = false, index = null) {
+    const modalTitle = document.getElementById("modalTitle");
+    currentUpdateIndex = index;
+
+    if (isEdit) {
+        const task = handler.taskList[index];
+        document.getElementById("title").value = task.title;
+        document.getElementById("desc").value = task.description;
+        document.getElementById("dueDate").value = task.dueDate;
+        modalTitle.textContent = "Update Task";
+    }
+    else {
+        document.getElementById("title").value = "";
+        document.getElementById("desc").value = "";
+        document.getElementById("dueDate").value = "";
+        modalTitle.textContent = "Add Task";
+    }
+
+    document.getElementById("updateModal").style.display = "block";
+}
+
+function addTask() {
+    openModal();
+}
+
+function editTask(index) {
+    openModal(true, index);
+}
+
+function closeModal() {
+    document.getElementById("updateModal").style.display = "none";
+    document.getElementById("err-msg").style.display = "none";
+    currentUpdateIndex = null;
+}
+
+async function saveTask() {
+    const title = document.getElementById("title").value.trim();
+    const desc = document.getElementById("desc").value.trim();
+    const dueDate = document.getElementById("dueDate").value;
+
+    if (title === "") {
+        const error = document.getElementById("err-msg");
+        error.textContent = "Title is required";
+        error.style.display = "block";
+        return;
+    }
+
+    if (currentUpdateIndex == null) {
+        const flag = await handler.addTask(title, desc, dueDate);
+        if (!flag) return;
+    }
+    else {
+        if (!handler.updateTask(currentUpdateIndex, title, desc, dueDate)) return;
+    }
+
+    handler.update();
+    closeModal();
+}
+
+function applyFilter() {
+    handler.update();
+}
+
+window.onload = () => {
+    handler.update();
+};
---
+++ task-manager/server.js
@@ -0,0 +1,154 @@
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { json } = require('stream/consumers');
+
+const dataFile = path.join(__dirname, 'data', 'tasks.json');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/tasks', (req, res) => {
+    fs.readFile(dataFile, (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to load tasks.json' });
+        }
+        const tasks = JSON.parse(data);
+        res.json(tasks);
+    });
+});
+
+app.post('/api/tasks', (req, res) => {
+    const newTask = req.body;
+    fs.readFile(dataFile, (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to load tasks.json' });
+        }
+
+        let tasks = [];
+        try {
+            tasks = JSON.parse(data || []);
+        }
+        catch (err) {
+            return res.status(500).json({ message: 'Invalid data' });
+        }
+
+        tasks.push(newTask);
+
+        fs.writeFile(dataFile, JSON.stringify(tasks, null, 4), (err) => {
+            if (err) {
+                return res.status(500).json({ message: 'Failed to save task' });
+            }
+
+            return res.status(201).json({ message: 'Task Added Successfully' });
+        });
+    });
+})
+
+app.patch('/api/tasks/:index', (req, res) => {
+    const index = req.params.index;
+    const { completed } = req.body;
+
+    fs.readFile(dataFile, (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to read tasks.json' });
+        }
+
+        let tasks = [];
+        try {
+            tasks = JSON.parse(data || []);
+        }
+        catch (err) {
+            return res.status(500).json({ message: 'Invalid data' });
+        }
+
+        tasks[index].completed = completed;
+
+        fs.writeFile(dataFile, JSON.stringify(tasks, null, 4), (err) => {
+            if (err) {
+                return res.status(500).json({ message: 'Failed to update task' });
+            }
+
+            return res.status(200).json({ message: 'Task updated successfuly' });
+        });
+    });
+});
+
+app.put('/api/tasks/:index', (req, res) => {
+    const index = req.params.index;
+    const { title, description, dueDate } = req.body;
+
+    fs.readFile(dataFile, (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to read tasks.json' });
+        }
+
+        let tasks = [];
+        try {
+            tasks = JSON.parse(data || []);
+        }
+        catch (err) {
+            return res.status(500).json({ message: 'Invalid data' });
+        }
+
+        if (index < 0 || index >= tasks.length) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        tasks[index].title = title;
+        tasks[index].description = description;
+        tasks[index].dueDate = dueDate;
+
+        fs.writeFile(dataFile, JSON.stringify(tasks, null, 4), (err) => {
+            if(err) {
+                return res.status(500).json({ message: 'Failed to update task' });
+            }
+
+            return res.status(200).json({ message: 'Task updated successfuly' });
+        });
+    });
+});
+
+app.delete('/api/tasks/:index', (req, res) => {
+    const index = req.params.index;
+
+    fs.readFile(dataFile, (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to read tasks.json' });
+        }
+
+        let tasks = [];
+        try {
+            tasks = JSON.parse(data || []);
+        }
+        catch (err) {
+            return res.status(500).json({ message: 'Invalid data' });
+        }
+
+        if (index < 0 || index >= tasks.length) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        tasks.splice(index,1);
+
+        fs.writeFile(dataFile, JSON.stringify(tasks, null, 4), (err) => {
+            if (err) {
+                return res.status(500).json({ message: 'Failed to delete task' });
+            }
+
+            return res.status(200).json({ message: 'Task deleted successfuly' });
+        });
+    });
+});
+
+app.listen(8080, () => {
+    console.log("http://localhost:8080/");
+});
+
